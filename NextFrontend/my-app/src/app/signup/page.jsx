8a61d0a486d994433851c2bdb65562a4de0871f5
yorkrem <489554@student.fontys.nl>
2024-05-21 12:49:36 +0200

    .

--- NextFrontend/my-app/src/app/signup/page.jsx
+++ NextFrontend/my-app/src/app/signup/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from 'react';
-import styles from '../styles/Signup.module.css';
+import styles from '../signup/Signup.module.css';
 
 export default function Signup() {
     const [formData, setFormData] = useState({
