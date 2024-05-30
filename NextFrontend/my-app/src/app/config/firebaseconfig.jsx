0a3fd4bfe951ce4a9877aace0864f35791587d33
yorkrem <489554@student.fontys.nl>
2024-05-30 14:30:32 +0200

    implemented authorization

--- NextFrontend/my-app/src/app/config/firebaseconfig.jsx
+++ NextFrontend/my-app/src/app/config/firebaseconfig.jsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth";
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +22,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 //const analytics = getAnalytics(app);
 
-export {createUserWithEmailAndPassword, auth, signInWithEmailAndPassword};
+export {createUserWithEmailAndPassword, auth, signInWithEmailAndPassword, signOut};
 
 export default app;
--- Services/AccountService/app/app.py
+++ Services/AccountService/app/app.py
@@ -1,33 +1,19 @@
-#from objects.account import Account
-#from managers.AccountManager import AccountManager
 from flask import jsonify, request
 from firebase_admin import auth
 from _init_ import app #, db
-
-#accountManager = AccountManager()
-
-#@app.route('/create', methods= ['POST'])
-#def createAccount():
-    #data = request.get_json()
-    #username = data.get('username')
-    #email = data.get('email')
-    #password = data.get('password')
-    #account = Account(username=username, email=email, password=password)
-    #accountManager.addAccount(account=account)
-    #db.session.add(account)
-    #db.session.commit()
-    #return str(account.getUsername())
 
 @app.route('/login', methods= ['POST'])
 def login():
     data = request.get_json()
     id_token = data.get('idToken')
+    if not id_token:
+        return jsonify({'error': 'ID token is missing'}), 400
     try:
         decoded_token = auth.verify_id_token(id_token)
         user_id = decoded_token['uid']
         return jsonify({'message': 'Token is valid', 'user_id': user_id}), 200
-    except auth.InvalidIdTokenError:
-        return jsonify({'error': 'Invalid token'}), 401
+    except Exception as e:
+        return jsonify({'error': str(e)}), 401
     
 if __name__ == '__main__':  # specify your desired port number here
     app.run(host="0.0.0.0", port=5001)
